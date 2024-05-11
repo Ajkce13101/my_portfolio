354dc78bb501dd191570a4dcaa1c26357f4f0210
Ajkce <kshettryaj13101@gmail.com>
2024-05-11 21:25:23 +1000

    Recreating portfolio website with vite

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="landing">
+        <div className="landing-bg">
+          Hello
+          {/* <Navbar />
+
+          <Home />
+          <About />
+          <Skills />
+          <Education /> */}
+        </div>
+      </div>
+      {/* <Portfolio />
+
+      <Contact /> */}
+      <div className="landing">
+        <div className="landing-bg">{/* <Footer /> */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
