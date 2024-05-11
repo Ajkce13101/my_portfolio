4d992f28845f56ab1e884b34ef5218d6028574e2
Ajkce <kshettryaj13101@gmail.com>
2024-05-12 00:39:06 +1000

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,34 @@ import "./App.scss";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Skills from "./Components/Skills/Skills";
+import Education from "./Components/Education/Education";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Footer from "./Components/Footer/Footer";
+import Contact from "./Components/Contact/Contact";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./Components/Navabr/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <div className="landing">
         <div className="landing-bg">
-          {/* <Navbar /> */}
+          <Navbar />
 
           <Home />
           <About />
           <Skills />
-          {/* <Education /> */}
+          <Education />
         </div>
       </div>
-      {/* <Portfolio />
+      <Portfolio />
 
-      <Contact /> */}
+      <Contact />
       <div className="landing">
-        <div className="landing-bg">{/* <Footer /> */}</div>
+        <div className="landing-bg">
+          <Footer />
+        </div>
       </div>
     </div>
   );
---
+++ src/img/index.js
@@ -0,0 +1,11 @@
+import Githubuser from "./Github.PNG";
+import Backroad from "./Backroad.PNG";
+import Comfy from "./Comfy.PNG";
+import Jobster from "./Jobster.PNG";
+import Jobify from "./jobify.PNG";
+import Ecommerce from "./Ecommerce.PNG";
+import Quill from "./quill.jpg";
+import Cinema from "./movie.JPG";
+
+
+export  {Githubuser, Backroad, Comfy, Jobster, Jobify, Ecommerce, Quill, Cinema};
